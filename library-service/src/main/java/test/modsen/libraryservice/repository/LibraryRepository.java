9596b5b791d1c37732027dbd5ef3efb3518caea5
Nikita Radyuk <radyuknikita02@gmail.com>
2024-04-01 12:30:50 +0300

    library-service edit logic, add get/return methods

--- library-service/src/main/java/test/modsen/libraryservice/repository/LibraryRepository.java
+++ library-service/src/main/java/test/modsen/libraryservice/repository/LibraryRepository.java
@@ -1,5 +1,6 @@
 package test.modsen.libraryservice.repository;
 
+import com.library.modsen.core.entities.BookEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,6 +13,6 @@ import java.util.UUID;
 @Repository
 public interface LibraryRepository extends JpaRepository<BookRecordEntity, UUID> {
     @Query("select BookEntity from BookEntity where BookEntity.status =: FREE")
-    Page<BookRecordEntity> getFreeBookRecordEntities();
+    Page<BookEntity> getFreeBookRecordEntities(Pageable pageable);
     BookRecordEntity getBookRecordEntityByUuid(UUID uuid);
 }
--- library-service/src/main/java/test/modsen/libraryservice/services/impl/LibraryServiceImpl.java
+++ library-service/src/main/java/test/modsen/libraryservice/services/impl/LibraryServiceImpl.java
@@ -1,33 +1,79 @@
 package test.modsen.libraryservice.services.impl;
 
+import com.library.modsen.core.entities.BookEntity;
+import com.library.modsen.core.enums.Status;
+import com.library.modsen.repository.BookRepository;
+import com.library.modsen.services.api.IBookService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import test.modsen.libraryservice.core.dto.BookFindDTO;
 import test.modsen.libraryservice.core.dto.BookRecordDTO;
+import test.modsen.libraryservice.core.dto.BookRecordGetDTO;
 import test.modsen.libraryservice.core.entity.BookRecordEntity;
 import test.modsen.libraryservice.core.exceptions.BookNotFoundException;
 import test.modsen.libraryservice.repository.LibraryRepository;
 import test.modsen.libraryservice.services.api.ILibraryService;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Service
 @Slf4j
 public class LibraryServiceImpl implements ILibraryService {
     private final LibraryRepository libraryRepository;
     private final ModelMapper modelMapper;
+    private final IBookService bookService;
+    private final BookRepository bookRepository;
 
-    public LibraryServiceImpl(LibraryRepository libraryRepository, ModelMapper modelMapper) {
+    public LibraryServiceImpl(LibraryRepository libraryRepository, ModelMapper modelMapper, IBookService bookService, BookRepository bookRepository) {
         this.libraryRepository = libraryRepository;
         this.modelMapper = modelMapper;
+        this.bookService = bookService;
+        this.bookRepository = bookRepository;
     }
 
     @Transactional(readOnly = true)
     @Override
-    public Page<BookRecordEntity> getFreeBooksPage() {
+    public Page<BookEntity> getFreeBooksPage(Pageable pageable) {
         log.info("Getting all free books");
-        return libraryRepository.getFreeBookRecordEntities();
+        return libraryRepository.getFreeBookRecordEntities(pageable);
+    }
+
+    @Transactional
+    @Override
+    public void getBookFromLibrary(BookRecordGetDTO bookRecordGetDTO){
+        BookRecordEntity bookRecord = new BookRecordEntity();
+        bookRecord.setUuid(bookRecordGetDTO.getBookUUID());
+        bookRecord.setGetTime(LocalDateTime.now());
+        bookRecord.setReturnTime(bookRecordGetDTO.getReturnTime());
+
+        BookEntity book = modelMapper.map(bookService.findByUuid(bookRecordGetDTO.getBookUUID()), BookEntity.class);
+
+        book.setStatus(Status.BUSY);
+
+        bookRepository.saveAndFlush(book);
+        libraryRepository.saveAndFlush(bookRecord);
+        log.info("The book was taken");
+    }
+
+    @Override
+    public void returnBookToLibrary(UUID bookUUID) {
+        BookRecordEntity bookRecordEntity = new BookRecordEntity();
+        bookRecordEntity.setUuid(bookUUID);
+        bookRecordEntity.setReturnTime(LocalDateTime.now());
+
+        BookEntity book = modelMapper.map(bookService.findByUuid(bookUUID), BookEntity.class);
+
+        book.setStatus(Status.BUSY);
+
+        bookRepository.saveAndFlush(book);
+        libraryRepository.saveAndFlush(bookRecordEntity);
+        log.info("The book was returned");
+
     }
 
     @Override
