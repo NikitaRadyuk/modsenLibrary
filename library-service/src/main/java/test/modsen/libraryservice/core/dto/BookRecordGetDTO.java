e2800be89b61af305c69ef1d9e7faabf38ecc2b6
Nikita Radyuk <radyuknikita02@gmail.com>
2024-04-01 12:29:57 +0300

    library-service add BookRecordGetDTO

---
+++ library-service/src/main/java/test/modsen/libraryservice/core/dto/BookRecordGetDTO.java
@@ -0,0 +1,20 @@
+package test.modsen.libraryservice.core.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BookRecordGetDTO {
+
+    @JsonProperty(value = "book_uuid")
+    private UUID bookUUID;
+    @JsonProperty(value = "return_time")
+    private LocalDateTime returnTime;
+}
